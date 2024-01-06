38107b9c54064e7765ec6115fd250465f66c16ee
JoaoVFM <joaovfmendonca@gmail.com>
2024-01-05 22:24:17 -0300

    adding all details

--- src/pages/CarachtersCacheContext.jsx
+++ src/pages/CarachtersCacheContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const CharacterCacheContext = createContext();
 
@@ -8,6 +8,69 @@ export const useCharacterCache = () => {
 
 export const CharacterCacheProvider = ({ children }) => {
   const [characterCache, setCharacterCache] = useState({});
+  const [charactersList, setCharactersList] = useState([]);
+  const [speciesList, setSpeciesList] = useState([]);
+  const [starshipsList, setStarshipsList] = useState([]);
+  const [planetsList, setPlanetsList] = useState([]);
+  const [vehiclesList, setVehiclesList] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/people");
+        const data = await response.json();
+        setCharactersList(data.results);
+      } catch (error) {
+        console.error("Error fetching characters:", error);
+      }
+    };
+
+    const fetchSpecies = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/species");
+        const data = await response.json();
+        setSpeciesList(data.results);
+      } catch (error) {
+        console.error("Error fetching species:", error);
+      }
+    };
+
+    const fetchStarships = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/starships");
+        const data = await response.json();
+        setStarshipsList(data.results);
+      } catch (error) {
+        console.error("Error fetching Starships:", error);
+      }
+    };
+
+    const fetchPlanets = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/planets");
+        const data = await response.json();
+        setPlanetsList(data.results);
+      } catch (error) {
+        console.error("Error fetching Planets:", error);
+      }
+    };
+
+    const fetchVehicles = async () => {
+      try {
+        const response = await fetch("https://swapi.dev/api/vehicles");
+        const data = await response.json();
+        setVehiclesList(data.results);
+      } catch (error) {
+        console.error("Error fetching Vehicles:", error);
+      }
+    };
+
+    fetchStarships();
+    fetchCharacters();
+    fetchSpecies();
+    fetchPlanets();
+    fetchVehicles();
+  }, []);
 
   const updateCache = (key, value) => {
     setCharacterCache((prevCache) => ({
@@ -20,9 +83,50 @@ export const CharacterCacheProvider = ({ children }) => {
     return characterCache[key];
   };
 
+  const getSpeciesFromCache = (key) => {
+    return (
+      speciesList.find((species) => species.url === key)?.name ||
+      "Espécie desconhecida"
+    );
+  };
+
+  const getStarshipsFromCache = (key) => {
+    return (
+      starshipsList.find((starships) => starships.url === key)?.name ||
+      "Starship desconhecida"
+    );
+  };
+
+  const getPlanetsFromCache = (key) => {
+    return (
+      planetsList.find((planet) => planet.url === key)?.name ||
+      "Planeta desconhecido"
+    );
+  };
+
+  const getVehiclesFromCache = (key) => {
+    return (
+      vehiclesList.find((vehicle) => vehicle.url === key)?.name ||
+      "Veículo desconhecido"
+    );
+  };
+
   return (
     <CharacterCacheContext.Provider
-      value={{ characterCache, updateCache, getCharacterFromCache }}
+      value={{
+        characterCache,
+        updateCache,
+        getCharacterFromCache,
+        getSpeciesFromCache,
+        getStarshipsFromCache,
+        getPlanetsFromCache,
+        getVehiclesFromCache,
+        charactersList,
+        speciesList,
+        starshipsList,
+        planetsList,
+        vehiclesList,
+      }}
     >
       {children}
     </CharacterCacheContext.Provider>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -11,7 +11,13 @@ const Details = () => {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(true);
-  const { getCharacterFromCache, updateCache } = useCharacterCache();
+  const {
+    getCharacterFromCache,
+    updateCache,
+    getStarshipsFromCache,
+    getPlanetsFromCache,
+    getVehiclesFromCache,
+  } = useCharacterCache();
 
   const replaceURIs = async (characters) => {
     try {
@@ -36,13 +42,106 @@ const Details = () => {
   useEffect(() => {
     const filmUrl = `https://swapi.dev/api/films/${id}`;
 
+    const getSpecies = async (speciesUrls) => {
+      try {
+        const speciesPromises = speciesUrls.map(async (speciesUrl) => {
+          if (getCharacterFromCache(speciesUrl)) {
+            return getCharacterFromCache(speciesUrl);
+          } else {
+            const response = await fetch(speciesUrl);
+            const speciesData = await response.json();
+            const speciesName = speciesData.name || "Espécie desconhecida";
+            updateCache(speciesUrl, speciesName);
+            return speciesName;
+          }
+        });
+        return await Promise.all(speciesPromises);
+      } catch (error) {
+        console.error("Erro ao obter detalhes das espécies:", error);
+        return speciesUrls.map(() => "Espécie desconhecida");
+      }
+    };
+
+    const getStarships = async (starshipsUrls) => {
+      try {
+        const starshipsPromises = starshipsUrls.map(async (starshipsUrl) => {
+          if (getCharacterFromCache(starshipsUrl)) {
+            return getCharacterFromCache(starshipsUrl);
+          } else {
+            const response = await fetch(starshipsUrl);
+            const starshipsData = await response.json();
+            const starshipsName =
+              starshipsData.name || "Starships desconhecida";
+            updateCache(starshipsUrl, starshipsName);
+            return starshipsName;
+          }
+        });
+        return await Promise.all(starshipsPromises);
+      } catch (error) {
+        console.error("Erro ao obter detalhes das starships:", error);
+        return starshipsUrls.map(() => "Starship desconhecida");
+      }
+    };
+
+    const getPlanets = async (planetsUrls) => {
+      try {
+        const planetsPromises = planetsUrls.map(async (planetUrl) => {
+          if (getCharacterFromCache(planetUrl)) {
+            return getCharacterFromCache(planetUrl);
+          } else {
+            const response = await fetch(planetUrl);
+            const planetData = await response.json();
+            const planetName = planetData.name || "Planeta desconhecido";
+            updateCache(planetUrl, planetName);
+            return planetName;
+          }
+        });
+        return await Promise.all(planetsPromises);
+      } catch (error) {
+        console.error("Erro ao obter detalhes dos planetas:", error);
+        return planetsUrls.map(() => "Planeta desconhecido");
+      }
+    };
+
+    const getVehicles = async (vehiclesUrls) => {
+      try {
+        const vehiclesPromises = vehiclesUrls.map(async (vehicleUrl) => {
+          if (getCharacterFromCache(vehicleUrl)) {
+            return getCharacterFromCache(vehicleUrl);
+          } else {
+            const response = await fetch(vehicleUrl);
+            const vehicleData = await response.json();
+            const vehicleName = vehicleData.name || "Veículo desconhecido";
+            updateCache(vehicleUrl, vehicleName);
+            return vehicleName;
+          }
+        });
+        return await Promise.all(vehiclesPromises);
+      } catch (error) {
+        console.error("Erro ao obter detalhes dos veículos:", error);
+        return vehiclesUrls.map(() => "Veículo desconhecido");
+      }
+    };
+
     const getMovie = async (filmUrl) => {
       try {
         setLoading(true);
         const response = await fetch(filmUrl);
         const data = await response.json();
+
         const charactersNames = await replaceURIs(data.characters);
-        const updatedMovie = { ...data, characters: charactersNames };
+        const speciesNames = await getSpecies(data.species);
+        const starshipsNames = await getStarships(data.starships);
+        const planetsNames = await getPlanets(data.planets);
+        const vehiclesNames = await getVehicles(data.vehicles);
+        const updatedMovie = {
+          ...data,
+          characters: charactersNames,
+          species: speciesNames,
+          starships: starshipsNames,
+          planets: planetsNames,
+          vehicles: vehiclesNames,
+        };
         setMovie(updatedMovie);
         setLoading(false);
       } catch (error) {
@@ -77,6 +176,50 @@ const Details = () => {
                     <span key={index}>
                       {character}
                       {index !== movie.characters.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="info">
+                <h3>Espécies:</h3>
+                <div className="horizontal-list">
+                  {movie.species.map((specie, index) => (
+                    <span key={index}>
+                      {specie}
+                      {index !== movie.species.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="info">
+                <h3>Starships:</h3>
+                <div className="horizontal-list">
+                  {movie.starships.map((starship, index) => (
+                    <span key={index}>
+                      {starship}
+                      {index !== movie.starships.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="info">
+                <h3>Planetas:</h3>
+                <div className="horizontal-list">
+                  {movie.planets.map((planet, index) => (
+                    <span key={index}>
+                      {planet}
+                      {index !== movie.planets.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="info">
+                <h3>Veículos:</h3>
+                <div className="horizontal-list">
+                  {movie.vehicles.map((vehicle, index) => (
+                    <span key={index}>
+                      {vehicle}
+                      {index !== movie.vehicles.length - 1 ? ", " : ""}
                     </span>
                   ))}
                 </div>
